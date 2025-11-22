afeeb752244745d7354d69a8d348dbd972255313
Bruletario <brunoalmeidaacademico@gmail.com>
2025-11-21 22:08:54 -0300

    Update app.py

--- CHATBOT/app.py
+++ CHATBOT/app.py
@@ -1,13 +1,1 @@
-# exemplo_basico.py
-from mangaba_ai import MangabaAgent
 
-# Criar agente
-agent = MangabaAgent()
-
-# Primeira conversa
-resposta = agent.chat("o que é um, pi?")
-print(resposta)
-
-# Continuar conversa
-resposta = agent.chat("quqantos numeros de pi voce consegue printar pra  mim?")
-print(resposta)
